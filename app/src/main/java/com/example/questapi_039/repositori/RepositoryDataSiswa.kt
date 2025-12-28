a1915fc026931cd65a6779fdd61f2dbd207a05db
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-12-28 14:44:43 +0700

    Menambahkan interface RepositoryDataSiswa untuk operasi CRUD data siswa

--- app/src/main/java/com/example/questapi_039/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_039/repositori/RepositoryDataSiswa.kt
@@ -5,22 +5,10 @@ import com.example.questapi_039.modeldata.DataSiswa
 import retrofit2.Response
 
 interface RepositoryDataSiswa {
-
     suspend fun getDataSiswa(): List<DataSiswa>
-
     suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void>
-
-    // suspend fun getSatuSiswa(id:Int): DataSiswa
-    // suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa): Response<Void>
-    // suspend fun hapusSatuSiswa(id:Int): Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void>
+    suspend fun hapusSatuSiswa(id: Int): Response<Void>
 }
-class JaringanRepositoryDataSiswa(
-    private val serviceApiSiswa: ServiceApiSiswa
-) : RepositoryDataSiswa {
 
-    override suspend fun getDataSiswa(): List<DataSiswa> =
-        serviceApiSiswa.getSiswa()
-
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> =
-        serviceApiSiswa.postSiswa(dataSiswa)
-}
