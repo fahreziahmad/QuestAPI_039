00a24cf08961b6170023ba282ef95a73c34abc6e
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-12-28 15:12:43 +0700

    Menambahkan EntryViewModel untuk pengelolaan input data siswa

--- app/src/main/java/com/example/questapi_039/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questapi_039/viewmodel/EntryViewModel.kt
@@ -5,47 +5,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.questapi_039.modeldata.DetailSiswa
+import com.example.questapi_039.modeldata.UIStateSiswa
+import com.example.questapi_039.modeldata.toDataSiswa
 import com.example.questapi_039.repositori.RepositoryDataSiswa
-import retrofit2.Response
 
-
-class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) :
-    ViewModel() {
-
-    var uiStateSiswa by mutableStateOf(UiStateSiswa())
-        private set
-
-    /* Fungsi untuk memvalidasi input */
-    private fun validasiInput(
-        uiState: DetailSiswa = uiStateSiswa.detailSiswa
-    ): Boolean {
-        return with(uiState) {
-            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
-        }
-    }
-
-    // Fungsi untuk menangani saat ada perubahan pada text input
-    fun updateUiState(detailSiswa: DetailSiswa) {
-        uiStateSiswa =
-            UiStateSiswa(
-                detailSiswa = detailSiswa,
-                isEntryValid = validasiInput(detailSiswa)
-            )
-    }
-
-    /* Fungsi untuk menyimpan data yang di-entry */
-    suspend fun addSiswa() {
-        if (validasiInput()) {
-            val sip: Response<Void> =
-                repositoryDataSiswa.postDataSiswa(
-                    uiStateSiswa.detailSiswa.toDataSiswa()
-                )
-
-            if (sip.isSuccessful) {
-                println("Sukses Tambah Data : ${sip.message()}")
-            } else {
-                println("Gagal tambah data : ${sip.errorBody()}")
-            }
-        }
-    }
-}
+class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
