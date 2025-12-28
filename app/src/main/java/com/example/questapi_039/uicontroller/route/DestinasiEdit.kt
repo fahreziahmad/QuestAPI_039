ef02ecf7921ba40217b4cd980535f343d86a807e
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-12-28 14:49:52 +0700

    Menambahkan DestinasiEdit untuk navigasi halaman edit siswa

--- app/src/main/java/com/example/questapi_039/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_039/uicontroller/route/DestinasiEdit.kt
@@ -1,2 +1,10 @@
 package com.example.questapi_039.uicontroller.route
 
+import com.example.questapi_039.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+    const val itemIdArg = "id"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
