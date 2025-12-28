30aeb84bfd6fdc116144029a7265b304eb2462cc
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-12-28 15:19:34 +0700

    Menambahkan DetailUiState untuk state UI halaman detail siswa

--- app/src/main/java/com/example/questapi_039/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_039/viewmodel/DetailViewModel.kt
@@ -1,2 +1,19 @@
 package com.example.questapi_039.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_039.modeldata.DataSiswa
+import com.example.questapi_039.repositori.RepositoryDataSiswa
+import com.example.questapi_039.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+sealed interface DetailUiState {
+    data class Success(val dataSiswa: DataSiswa) : DetailUiState
+    object Error : DetailUiState
+    object Loading : DetailUiState
+}
+
