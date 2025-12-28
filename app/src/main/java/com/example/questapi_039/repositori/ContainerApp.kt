db69451b3415e994a4bf2d55449fcf914ab023e6
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-12-28 14:41:30 +0700

    Menambahkan ContainerApp dan konfigurasi dasar Retrofit untuk API Siswa

--- app/src/main/java/com/example/questapi_039/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi_039/repositori/ContainerApp.kt
@@ -20,37 +20,3 @@ class DefaultContainerApp : ContainerApp {
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
-
-    private val klien = OkHttpClient.Builder()
-        .addInterceptor(logging)
-        .build()
-
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(baseurl)
-        .addConverterFactory(
-            Json {
-                ignoreUnknownKeys = true
-                isLenient = true
-            }.asConverterFactory("application/json".toMediaType())
-        )
-        .client(klien)
-        .build()
-
-    private val retrofitService: ServiceApiSiswa by lazy {
-        retrofit.create(ServiceApiSiswa::class.java)
-    }
-
-    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
-        JaringanRepositoryDataSiswa(retrofitService)
-    }
-}
-
-class AplikasiDataSiswa : Application() {
-    lateinit var container: ContainerApp
-        private set
-
-    override fun onCreate() {
-        super.onCreate()
-        container = DefaultContainerApp()
-    }
-}
