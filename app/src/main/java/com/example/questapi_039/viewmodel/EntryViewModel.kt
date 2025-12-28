1da209f85ea69e54a1a82028a504bb31e4f4f157
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-12-28 15:13:22 +0700

    Menambahkan state UI dan validasi input pada EntryViewModel

--- app/src/main/java/com/example/questapi_039/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/questapi_039/viewmodel/EntryViewModel.kt
@@ -10,3 +10,12 @@ import com.example.questapi_039.modeldata.toDataSiswa
 import com.example.questapi_039.repositori.RepositoryDataSiswa
 
 class EntryViewModel(private val repositoryDataSiswa: RepositoryDataSiswa) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
