ad27e93d7a0215cd2fc6434b166d80548edf75ef
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-12-28 14:45:24 +0700

    Menambahkan JaringanRepositoryDataSiswa sebagai implementasi RepositoryDataSiswa

--- app/src/main/java/com/example/questapi_039/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_039/repositori/RepositoryDataSiswa.kt
@@ -12,3 +12,11 @@ interface RepositoryDataSiswa {
     suspend fun hapusSatuSiswa(id: Int): Response<Void>
 }
 
+class JaringanRepositoryDataSiswa(private val serviceApiSiswa: ServiceApiSiswa) : RepositoryDataSiswa {
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSiswaById(id)
+    override suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): Response<Void> = serviceApiSiswa.updateSiswa(id, dataSiswa)
+    override suspend fun hapusSatuSiswa(id: Int): Response<Void> = serviceApiSiswa.deleteSiswa(id)
+
+}
