1e06b5ccc637bf0c2eaf4a5f1ebd89e953d75c02
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2025-12-28 15:23:59 +0700

    Menambahkan EditViewModel untuk pengelolaan update data siswa

--- app/src/main/java/com/example/questapi_039/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_039/viewmodel/EditViewModel.kt
@@ -1,2 +1,20 @@
 package com.example.questapi_039.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_039.modeldata.DetailSiswa
+import com.example.questapi_039.modeldata.UIStateSiswa
+import com.example.questapi_039.modeldata.toDataSiswa
+import com.example.questapi_039.modeldata.toUIStateSiswa // TAMBAHKAN IMPORT INI
+import com.example.questapi_039.repositori.RepositoryDataSiswa
+import com.example.questapi_039.uicontroller.route.DestinasiEdit
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryDataSiswa: RepositoryDataSiswa
+) : ViewModel() {
